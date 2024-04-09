ce067cf4a5ac5e4f7b2c60f273f0a5ec73f5a823
aguemes-appvant <aguemes@appvant.com>
2024-04-09 10:52:23 -0600

    ADD clean map

--- taller-tec/src/App.js
+++ taller-tec/src/App.js
@@ -1,32 +1,8 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { setMapScriptLoadedHnd } from './Tools/Configuration/configurationHandler';
-import Location from './Components/Location/Location';
-const mapTk = 'AIzaSyC4s13ZpukZUK_juf6U5KnHwOHCfa8H6pM'
-
-const loadScript = (url) => {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = url;
-  const head = document.querySelector('head');
-  head.appendChild(script);
-  return new Promise(resolve => {
-      script.onload = resolve;
-  });
-}
 
 function App() {
-
-  window.setMapScriptLoadedCb = () => {
-    setMapScriptLoadedHnd(true);
-  }
-
-  useEffect(() => {
-    if (!window.google){
-      loadScript(`https://maps.googleapis.com/maps/api/js?v=3&key=${mapTk}&loading=async&callback=setMapScriptLoadedCb`)
-    }
-  }, []);
 
   return (
     <div className="App">
@@ -44,7 +20,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Location></Location>
     </div>
   );
 }
